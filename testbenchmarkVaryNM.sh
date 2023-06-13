78e952a4d030d16653ae9ed7699973e2dd6a6ed2
jiaweizheng1 <jiaweizheng416528@gmail.com>
2023-06-13 13:12:55 -0700

    gridsearch.txt

--- testbenchmarkVaryNM.sh
+++ testbenchmarkVaryNM.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-for (( i = 32; i <= 1024; i += 32))
+for (( i = 32; i <= 1024; i *= 2))
 do
-    for ((j = 32; j <= 1024; j += 32))
+    for ((j = 32; j <= 1024; j *= 2))
     do
         echo `./benchmark $i $j`
     done
